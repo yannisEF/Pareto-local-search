0da5b7f9bdaa137e2bc934a36f4f07d302b49e3f
y_elrh <49323355+sohio92@users.noreply.github.com>
2021-12-21 13:55:15 +0100

    Added PLS3, previous issue seems to stem from init_pop method

--- pls3.py
+++ pls3.py
@@ -1,3 +1,4 @@
+from os import popen
 import random
 import matplotlib.pyplot as plt
 
@@ -12,32 +13,45 @@ class PLS3(PLS1):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.init_S = 60 if "init_S" not in kwargs.keys() else kwargs["init_S"]
+        self.init_S = 1 if "init_S" not in kwargs.keys() else kwargs["init_S"]
 
     def get_init_pop(self, instance):
-        pop = []
+        """
+        Initialize the population with a randomly generated weighted choice
+        """
 
-        while len(pop) < self.init_S:
-            copy_obj = instance["Objects"][:]
-            q = random.random()
+        pop_index = []
+        n_dim = len(instance["Objects"][1])
+
+        while len(pop_index) < self.init_S:
+            index_objects = list(filter(lambda i: instance["Objects"][0][i] <= instance['W'], range(len(instance["Objects"][0]))))
+            
+            # Randomly sample a number of weights
+            q = [random.random() for _ in range(n_dim)]
+            q = [x / sum(q) for x in q]
             S = 0
             
-            indiv = []
-            while S <= instance['W'] and len(copy_obj) > 0:
-                r_obj = [(q * x["v1"] + (1-q) * x["v2"]) / x["w"] for x in copy_obj]
+            indiv_index = []
+            while S <= instance['W'] and len(index_objects) > 0:
+                # Define the objects' weights
+                r_obj = [sum(q[i] * instance["Objects"][1][i][v] for i in range(n_dim)) / instance["Objects"][0][v] for v in index_objects]
                 r_obj = [r/sum(r_obj) for r in r_obj]
 
-                new_obj = random.choices(copy_obj, weights=r_obj, k=1)[0]
-                copy_obj.remove(new_obj)
-
-                if new_obj['w'] + S <= instance['W']:   
-                    indiv.append(new_obj)
-                    S += new_obj['w']
-
-            if len(indiv) == 0: raise ValueError("init_S might be to initialize the search")
-            else:   pop.append(indiv)
+                # Randomly choose a new object
+                new_index_obj = random.choices(index_objects, weights=r_obj, k=1)[0]
+                index_objects.remove(new_index_obj)
                 
-        return pop
+                # Add it to the population
+                indiv_index.append(new_index_obj)
+                S += instance["Objects"][0][new_index_obj]
+                
+                # Filter the backpack so that only the compatible weigths remain
+                index_objects = list(filter(lambda i: instance["Objects"][0][i] <= instance['W'], index_objects))
+                
+            if len(indiv_index) == 0: raise ValueError("init_S might be to big to initialize the search")
+            else:   pop_index.append(indiv_index)
+                
+        return pop_index
     
 if __name__ == "__main__":
     pls3 = PLS3(nb_files=1, nb_tries=1)
