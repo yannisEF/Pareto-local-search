afa12e8240330f5acb6e63eb6587ccb2394c70ff
pqs27 <peiqisun94@gmail.com>
2022-01-23 14:03:16 +0100

    Update utils_elicitation.py

--- Utils/utils_elicitation.py
+++ Utils/utils_elicitation.py
@@ -1,6 +1,6 @@
 import gurobipy as gp
 
-from agregation_functions import weighted_sum, OWA, choquet
+from agregation_functions import weighted_sum, OWA, choquet, capacity
 
 
 def compute_pmr(P, agregation_function, x, y):
@@ -12,36 +12,53 @@ def compute_pmr(P, agregation_function, x, y):
     model = gp.Model()
     model.setParam('OutputFlag', 0)
 
-    # Variables
-    #   All the possible weights of the agregation function
-    weights = [model.addVar(vtype='C', name='w{}'.format(i)) for i in range(dim)]
-    # f(x) = w1 * x1 + w2 * x2 + .... 
+        # Variables
+        #   All the possible weights of the agregation function
+    if agregation_function == OWA or agregation_function == weighted_sum :
+        weights = [model.addVar(vtype='C', name='w{}'.format(i)) for i in range(dim)]
     
-    # Objective
-    #   max f(w, y) - f(w, x)
-    f = lambda a: agregation_function(weights, a)
-    model.setObjective(f(y) - f(x), gp.GRB.MAXIMIZE)
+        # Objective
+        #   max f(w, y) - f(w, x)
+        f = lambda a: agregation_function(weights, a)
+        model.setObjective(f(y) - f(x), gp.GRB.MAXIMIZE)
 
-    # Constraints
-    #   All preferences have to be respected
-    model.addConstrs(f(a) >= f(b) for a,b in P)
-    #   Weights must sum to 1
-    model.addConstr(sum(weights) == 1)
-    #   Weights between 0 and 1:
+        # Constraints
+        #   All preferences have to be respected
+        model.addConstrs(f(a) >= f(b) for a,b in P)
+        #   Weights must sum to 1
+        model.addConstr(sum(weights) == 1)
+        #   Weights between 0 and 1:
     
-    if f == weighted_sum:
-        for w in weights:
-            model.addConstr(w <= 1)
-            model.addConstr(w >= 0)
-    elif f == OWA:
-        for i in range(len(weights)-1):
-            model.addConstr(weights[i] >= weights[i+1])
-            model.addConstr(weights[i] <= 1)
-            model.addConstr(weights[i+1] >= 0)
-
+        if f == weighted_sum:
+            for w in weights:
+                model.addConstr(w <= 1)
+                model.addConstr(w >= 0)
+        elif f == OWA:
+            for i in range(len(weights)-1):
+                model.addConstr(weights[i] >= weights[i+1])
+                model.addConstr(weights[i] <= 1)
+                model.addConstr(weights[i+1] >= 0)
+    if agregation_function == choquet:
+        weights = [model.addVar(vtype='C', name='w{}'.format(i)) for i in range(dim**2)]
+        f = lambda a: agregation_function(weights, a)
+        model.setObjective(f(y) - f(x), gp.GRB.MAXIMIZE)
+        c = capacity(dim).set_label
+        model.addConstrs(f(a) >= f(b) for a,b in P)
+        # constraints of capacity
+        weights[0] = 0
+        weights[-1] = 1
+        for i in range(dim**2-1):
+            for j in range(i+1,dim**2-1):
+                s1 , s2 = c[i] & c[j] , c[i] | c[j]
+                # constraints of supermodularity capacity
+                model.addConstr(weights[i] + weights[j] <= weights[c.index(s1)] + weights[c.index(s2)])
+                if (c[i] < c[j]):
+                    # constraints of monotonicity of capacity
+                    model.addConstr(weights[i] <= weights[j])
+                    model.addConstr(weights[i] >= 0)
+                    model.addConstr(weights[j] <= 1)
     # Solve the model
     model.optimize()
-
     return model.objVal
 
 def compute_mr(P, agregation_function, x_set, x, dominated):
