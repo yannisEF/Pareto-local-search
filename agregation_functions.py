3ac2e8791df4f2be893fa6beeb22b61972f2d665
pqs27 <peiqisun94@gmail.com>
2022-01-23 13:15:16 +0100

    Update agregation_functions.py

--- agregation_functions.py
+++ agregation_functions.py
@@ -4,8 +4,8 @@ def weighted_sum(weights, x_vector):
 
 
 def OWA(weights, x_vector):
+    x_vector = list(x_vector)
     x_vector.sort()
-    weights.sort(reverse = True)
     return sum(weights[i] * x_vector[i] for i in range(len(weights))) 
 
 
